9dfa56bcc1aef7203095b1ba776a4ec23c943dc7
Mattias Åkesson <mattias.akesson1@inter.ikea.com>
2024-04-10 10:55:58 +0200

    Remove env stuff

--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable env {
-  description = "The environment we're deploying to"
-  type        = string
-}
-
-variable app_sku {
-  description = "The SKU of the plan the app is run on"
-  type        = string
-}
--- terraform/webapp.tf
+++ terraform/webapp.tf
@@ -2,29 +2,3 @@ resource "azurerm_resource_group" "awesome_app" {
   name     = "rg-awesome-app-${var.env}"
   location = "North Europe"
 }
-
-resource "azurerm_service_plan" "awesome_app" {
-  name                = "plan-awesome-app-${var.env}"
-  resource_group_name = azurerm_resource_group.awesome_app.name
-  location            = azurerm_resource_group.awesome_app.location
-  os_type             = "Linux"
-  sku_name            = var.app_sku
-}
-
-resource "azurerm_linux_web_app" "awesome_app" {
-  name                = "webapp-awesome-app-${var.env}"
-  resource_group_name = azurerm_resource_group.awesome_app.name
-  location            = azurerm_service_plan.awesome_app.location
-  service_plan_id     = azurerm_service_plan.awesome_app.id
-
-  https_only = true
-
-  site_config {
-    always_on = false
-    application_stack {
-      docker_registry_url = "https://index.docker.io"
-      docker_image_name = "dergons/demo-app:latest"
-    }
-  }
-}
-
